1f18c7376c6cb6cbf74ef1e606c1e5c0432cb9e7
Nicolas Gallagher <nicolasgallagher@gmail.com>
2011-10-20 02:31:21 +0100

    Add async script loading snippet

---
+++ async-script-loading/async-script-loading.js
@@ -0,0 +1,35 @@
+/*
+ * Optimised asynchronous loading of cross-domain scripts
+ *
+ * Adapted from the function-based method described in http://www.phpied.com/social-button-bffs/
+ * A document fragment is used to minimise the number of times objects are written to the DOM.
+ * Supports the optional inclusion of an `id` for any supplied script.
+ *
+ * N.B. When used to load the Google Analytics or Facebook scripts, be sure to include the
+ * necessary variables prior to this function.
+ */
+
+(function(doc, script) {
+    var js,
+        fjs = doc.getElementsByTagName(script)[0],
+        frag = doc.createDocumentFragment(),
+        add = function(url, id) {
+            if (doc.getElementById(id)) {return;}
+            js = doc.createElement(script);
+            js.src = url;
+            js.id = id || null;
+            frag.appendChild(js);
+        };
+
+    // Include any scripts (e.g. SDKs, social sharing buttons) that you want to load
+    // asynchronously and an optional `id`
+
+    // Example
+    // add('http://example.com/script.js', 'examplesdk');
+    // Google Analytics
+    // add(('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js', 'ga');
+    // Facebook SDK
+    // add('//connect.facebook.net/en_US/all.js', 'facebook-jssdk');
+
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
