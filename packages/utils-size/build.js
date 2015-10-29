47641d5030cf38367439e3c063b87d268caf02f5
Simon Smith <me@simonsmith.io>
2015-10-29 20:36:34 +0000

    Replace suitcss-preprocessor with postcss

---
+++ packages/utils-size/build.js
@@ -0,0 +1,17 @@
+const postcss = require('postcss');
+const bemLinter = require('postcss-bem-linter');
+const reporter = require('postcss-reporter');
+
+module.exports = {
+  use: [
+    "postcss-import",
+    "postcss-calc",
+    "postcss-custom-media",
+    "autoprefixer"
+  ],
+  "postcss-import": {
+    transform(css) {
+      return postcss([bemLinter, reporter]).process(css).css;
+    }
+  }
+};
