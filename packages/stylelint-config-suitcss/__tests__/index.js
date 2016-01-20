1079017a8c87214176920e1ae50914dfdf7622d8
jeddy3 <jeddy3@users.noreply.github.com>
2016-01-20 15:30:36 +0000

    Use ava for tests

--- packages/stylelint-config-suitcss/__tests__/index.js
+++ packages/stylelint-config-suitcss/__tests__/index.js
@@ -1,39 +1,109 @@
 import config from "../"
 import stylelint from "stylelint"
-import test from "tape"
+import test from "ava"
 
-test("basic properties of config", t => {
-  t.ok(isObject(config.rules), "rules is object")
-  t.end()
-})
+const validCss = (
+`/**
+ * Multi-line comment
+ */
 
-function isObject(obj) {
-  return typeof obj === "object" && obj !== null
+.selector-1,
+.selector-2,
+.selector-3[type="text"] {
+  background: linear-gradient(#fff, rgba(0, 0, 0, 0.8));
+  box-sizing: border-box;
+  color: #333;
+  display: block;
 }
 
-const css = (
+.selector-a,
+.selector-b {
+  padding: 10px !important;
+  top: calc(calc(1em * 2) / 3);
+}
+
+.selector-x { width: 10%; }
+.selector-y { width: 20%; }
+.selector-z { width: 30%; }
+
+/* Single-line comment */
+
+@media (min-width >= 60em) {
+  .selector {
+    transform: translate(1, 1) scale(3);
+  }
+}
+
+@media (min-orientation: portrait), projection and (color) {
+  .selector-i + .selector-ii {
+    background: color(rgb(0, 0, 0) lightness(50%));
+    font-family: helvetica, arial, sans-serif;
+  }
+}
+
+@media screen and screen and (min-resolution: 192dpi),
+  screen and (min-resolution: 2dppx) {
+  .selector {
+    background-image:
+      repeating-linear-gradient(
+        -45deg,
+        transparent,
+        #fff 25px,
+        rgba(255, 255, 255, 1) 50px
+      );
+    box-shadow:
+      0 1px 1px #000,
+      0 1px 0 #fff,
+      2px 2px 1px 1px #ccc inset;
+    height: 10rem;
+    margin: 10px;
+    margin-bottom: 5px;
+  }
+
+  .selector::after {
+    background-image: url("x.svg");
+    content: "→";
+  }
+}
+
+`)
+
+const invalidCss = (
 `a {
   top: .2em;
 }
 
 `)
 
-stylelint.lint({
-  code: css,
-  config: config,
-})
-.then(checkResult)
-.catch(function (err) {
-  console.error(err.stack)
+test("expected no warnings with valid css", t => {
+  stylelint.lint({
+    code: validCss,
+    config: config,
+  })
+  .then(data => {
+    const { errored, results } = data
+    const { warnings } = results[0]
+    t.notOk(errored, "no errored")
+    t.is(warnings.length, 0, "flags no warnings")
+  })
+  .catch(function (err) {
+    console.error(err.stack)
+  })
 })
 
-function checkResult(data) {
-  const { errored, results } = data
-  const { warnings } = results[0]
-  test("expected warnings", t => {
-    t.ok(errored, "errored")
-    t.equal(warnings.length, 1, "flags one warning")
-    t.equal(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
-    t.end()
+test("expected a warning with invalid css", t => {
+  stylelint.lint({
+    code: invalidCss,
+    config: config,
   })
-}
+  .then(data => {
+    const { errored, results } = data
+    const { warnings } = results[0]
+    t.ok(errored, "errored")
+    t.is(warnings.length, 1, "flags one warning")
+    t.is(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
+  })
+  .catch(function (err) {
+    console.error(err.stack)
+  })
+})
